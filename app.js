bfb93deae9a86be381b7376f410c125f9d782da7
Arslan123Iqbal <arslaniqbalmgt@gmail.com>
2023-05-13 16:31:47 +0500

    3js Starter

---
+++ app.js
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import sFragment from './shaders/fragment.glsl';
+import sVertex from './shaders/vertex.glsl';
+export default class Sketch {
+  constructor() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.getElementById('container').appendChild(this.renderer.domElement);
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+
+    this.scene = new THREE.Scene();
+
+    this.addMesh();
+    this.time = 0;
+    this.render();
+  }
+
+  addMesh() {
+    this.geometry = new THREE.PlaneGeometry(1, 1);
+    this.material = new THREE.MeshNormalMaterial({side:THREE.DoubleSide});
+    this.material = new THREE.ShaderMaterial({
+        vertexShader:sVertex,
+        fragmentShader:sFragment,
+        uniforms:{
+            progress: {type:"f", value:0}
+        },
+        side:THREE.DoubleSide
+    })
+    this.mesh = new THREE.Points(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  render() {
+    this.time++;
+    this.mesh.rotation.x += 0.02;
+    this.mesh.rotation.y = this.time / 1000;
+
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+new Sketch();
